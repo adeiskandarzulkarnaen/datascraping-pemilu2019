383252f0f1430dd21c601b195d40a767dd607ec7
adeiskandarzulkarnaen <adeiskandarzulkarnaen@gmail.com>
2023-09-15 06:46:21 +0700

    add update kode lokasi jawa tengah

---
+++ src/mapping/_result/33.sql
@@ -0,0 +1,130 @@
+
+-- init
+UPDATE wilayah SET kode = '33.02.22', nama = 'BATURRADEN' WHERE kode_wilayah_kpu = '32676.32986.33286';
+UPDATE wilayah SET kode = '33.04.02', nama = 'PURWOREJA KLAMPOK' WHERE kode_wilayah_kpu = '32676.33603.33620';
+UPDATE wilayah SET kode = '33.04.04',  nama = 'PURWANEGARA' WHERE kode_wilayah_kpu = '32676.33603.33646';
+UPDATE wilayah SET kode = '33.19.02', nama = 'KOTA KUDUS' WHERE kode_wilayah_kpu = '32676.38564.38581';
+
+UPDATE wilayah SET kode = '33.02.22.2009' WHERE kode_wilayah_kpu = '32676.32986.33286.33295'
+UPDATE wilayah SET kode = '33.04.01.2005' WHERE kode_wilayah_kpu = '32676.33603.33604.33609';
+UPDATE wilayah SET kode = '33.04.01.2006' WHERE kode_wilayah_kpu = '32676.33603.33604.33610';
+UPDATE wilayah SET kode = '33.05.06.2018' WHERE kode_wilayah_kpu = '32676.33902.34014.34032';
+UPDATE wilayah SET kode = '33.05.10.2017' WHERE kode_wilayah_kpu = '32676.33902.34106.34123';
+UPDATE wilayah SET kode = '33.05.13.2002' WHERE kode_wilayah_kpu = '32676.33902.34173.34175';
+UPDATE wilayah SET kode = '33.05.14.2003' WHERE kode_wilayah_kpu = '32676.33902.34187.34190';
+UPDATE wilayah SET kode = '33.05.15.2016' WHERE kode_wilayah_kpu = '32676.33902.34209.34225';
+UPDATE wilayah SET kode = '33.05.18.2013' WHERE kode_wilayah_kpu = '32676.33902.34268.34281';
+UPDATE wilayah SET kode = '33.05.21.2004' WHERE kode_wilayah_kpu = '32676.33902.34312.34316';
+UPDATE wilayah SET kode = '33.06.02.2008' WHERE kode_wilayah_kpu = '32676.34389.34423.34431';
+UPDATE wilayah SET kode = '33.06.02.2044' WHERE kode_wilayah_kpu = '32676.34389.34423.34467';
+UPDATE wilayah SET kode = '33.06.03.2038' WHERE kode_wilayah_kpu = '32676.34389.34481.34519';
+UPDATE wilayah SET kode = '33.06.05.2019' WHERE kode_wilayah_kpu = '32676.34389.34540.34559';
+UPDATE wilayah SET kode = '33.06.09.2002' WHERE kode_wilayah_kpu = '32676.34389.34643.34651';
+UPDATE wilayah SET kode = '33.06.09.2021' WHERE kode_wilayah_kpu = '32676.34389.34643.34664';
+UPDATE wilayah SET kode = '33.06.10.2013' WHERE kode_wilayah_kpu = '32676.34389.34671.34684';
+UPDATE wilayah SET kode = '33.06.10.2037' WHERE kode_wilayah_kpu = '32676.34389.34671.34708';
+UPDATE wilayah SET kode = '33.06.12.2038' WHERE kode_wilayah_kpu = '32676.34389.34763.34801';
+
+UPDATE wilayah SET kode = '33.07.08.2014' WHERE kode_wilayah_kpu = '32676.34900.35041.35055';
+UPDATE wilayah SET kode = '33.07.09.1008' WHERE kode_wilayah_kpu = '32676.34900.35063.35071';
+UPDATE wilayah SET kode = '33.08.10.1009' WHERE kode_wilayah_kpu = '32676.35181.35328.35337';
+UPDATE wilayah SET kode = '33.08.12.2009' WHERE kode_wilayah_kpu = '32676.35181.35358.35367';
+UPDATE wilayah SET kode = '33.08.19.2020' WHERE kode_wilayah_kpu = '32676.35181.35511.35531';
+UPDATE wilayah SET kode = '33.08.20.2003' WHERE kode_wilayah_kpu = '32676.35181.35533.35536';
+
+/* fix 33.09.02 change to 33.09.20 */
+UPDATE wilayah SET kode = '33.09.20.2001' WHERE kode_wilayah_kpu = '32676.35575.35587.35588';
+UPDATE wilayah SET kode = '33.09.20.2002' WHERE kode_wilayah_kpu = '32676.35575.35587.35599';
+UPDATE wilayah SET kode = '33.09.20.2003' WHERE kode_wilayah_kpu = '32676.35575.35587.35600';
+UPDATE wilayah SET kode = '33.09.20.2004' WHERE kode_wilayah_kpu = '32676.35575.35587.35601';
+UPDATE wilayah SET kode = '33.09.20.2005' WHERE kode_wilayah_kpu = '32676.35575.35587.35602';
+UPDATE wilayah SET kode = '33.09.20.2006' WHERE kode_wilayah_kpu = '32676.35575.35587.35603';
+UPDATE wilayah SET kode = '33.09.20.2007' WHERE kode_wilayah_kpu = '32676.35575.35587.35604';
+UPDATE wilayah SET kode = '33.09.20.2008' WHERE kode_wilayah_kpu = '32676.35575.35587.35605';
+UPDATE wilayah SET kode = '33.09.20.2009' WHERE kode_wilayah_kpu = '32676.35575.35587.35606';
+UPDATE wilayah SET kode = '33.09.20.2010' WHERE kode_wilayah_kpu = '32676.35575.35587.35607';
+
+/* fix 33.09.04 change to 33.09.21 */
+UPDATE wilayah SET kode = '33.09.21.2001' WHERE kode_wilayah_kpu = '32676.35575.35624.35625';
+UPDATE wilayah SET kode = '33.09.21.2002' WHERE kode_wilayah_kpu = '32676.35575.35624.35626';
+UPDATE wilayah SET kode = '33.09.21.2003' WHERE kode_wilayah_kpu = '32676.35575.35624.35627';
+UPDATE wilayah SET kode = '33.09.21.2004' WHERE kode_wilayah_kpu = '32676.35575.35624.35628';
+UPDATE wilayah SET kode = '33.09.21.2005' WHERE kode_wilayah_kpu = '32676.35575.35624.35629';
+UPDATE wilayah SET kode = '33.09.21.2006' WHERE kode_wilayah_kpu = '32676.35575.35624.35630';
+UPDATE wilayah SET kode = '33.09.21.2007' WHERE kode_wilayah_kpu = '32676.35575.35624.35631';
+UPDATE wilayah SET kode = '33.09.21.2008' WHERE kode_wilayah_kpu = '32676.35575.35624.35632';
+UPDATE wilayah SET kode = '33.09.21.2009' WHERE kode_wilayah_kpu = '32676.35575.35624.35633';
+UPDATE wilayah SET kode = '33.09.21.2010' WHERE kode_wilayah_kpu = '32676.35575.35624.35634';
+
+/* fix 33.09.04 change to 33.09.22 */
+UPDATE wilayah SET kode = '33.09.22.2001' WHERE kode_wilayah_kpu = '32676.35575.35832.35833';
+UPDATE wilayah SET kode = '33.09.22.2002' WHERE kode_wilayah_kpu = '32676.35575.35832.35841';
+UPDATE wilayah SET kode = '33.09.22.2003' WHERE kode_wilayah_kpu = '32676.35575.35832.35842';
+UPDATE wilayah SET kode = '33.09.22.2004' WHERE kode_wilayah_kpu = '32676.35575.35832.35843';
+UPDATE wilayah SET kode = '33.09.22.2005' WHERE kode_wilayah_kpu = '32676.35575.35832.35844';
+UPDATE wilayah SET kode = '33.09.22.2006' WHERE kode_wilayah_kpu = '32676.35575.35832.35845';
+UPDATE wilayah SET kode = '33.09.22.2007' WHERE kode_wilayah_kpu = '32676.35575.35832.35847';
+UPDATE wilayah SET kode = '33.09.22.2008' WHERE kode_wilayah_kpu = '32676.35575.35832.35848';
+UPDATE wilayah SET kode = '33.09.22.2009' WHERE kode_wilayah_kpu = '32676.35575.35832.35849';
+UPDATE wilayah SET kode = '33.09.22.2010' WHERE kode_wilayah_kpu = '32676.35575.35832.35850';
+
+
+UPDATE wilayah SET kode = '33.09.03.2002' WHERE kode_wilayah_kpu = '32676.35575.35608.35610';
+UPDATE wilayah SET kode = '33.09.05.1002' WHERE kode_wilayah_kpu = '32676.35575.35645.35647';
+UPDATE wilayah SET kode = '33.14.02.2006' WHERE kode_wilayah_kpu = '32676.36985.37001.37007';
+
+UPDATE wilayah SET kode = '33.15.03.2015' WHERE kode_wilayah_kpu = '32676.37214.37248.37263';
+UPDATE wilayah SET kode = '33.15.11.2010' WHERE kode_wilayah_kpu = '32676.37214.37372.37382';
+UPDATE wilayah SET kode = '33.17.03.2016' WHERE kode_wilayah_kpu = '32676.37826.37863.37879';
+UPDATE wilayah SET kode = '33.17.06.2012' WHERE kode_wilayah_kpu = '32676.37826.37920.37932';
+UPDATE wilayah SET kode = '33.17.11.2001' WHERE kode_wilayah_kpu = '32676.37826.38048.38049';
+UPDATE wilayah SET kode = '33.17.11.2004' WHERE kode_wilayah_kpu = '32676.37826.38048.38052';
+UPDATE wilayah SET kode = '33.18.05.2011' WHERE kode_wilayah_kpu = '32676.38136.38222.38233';
+UPDATE wilayah SET kode = '33.18.12.2014' WHERE kode_wilayah_kpu = '32676.38136.38393.38407';
+UPDATE wilayah SET kode = '33.18.12.2016' WHERE kode_wilayah_kpu = '32676.38136.38393.38409';
+UPDATE wilayah SET kode = '33.18.14.2010' WHERE kode_wilayah_kpu = '32676.38136.38424.38434';
+UPDATE wilayah SET kode = '33.18.16.2021' WHERE kode_wilayah_kpu = '32676.38136.38459.38480';
+UPDATE wilayah SET kode = '33.20.03.2011' WHERE kode_wilayah_kpu = '32676.38706.38739.38750';
+UPDATE wilayah SET kode = '33.20.12.2012' WHERE kode_wilayah_kpu = '32676.38706.38858.38870';
+UPDATE wilayah SET kode = '33.21.09.2017' WHERE kode_wilayah_kpu = '32676.38917.39069.39086';
+UPDATE wilayah SET kode = '33.21.10.2013' WHERE kode_wilayah_kpu = '32676.38917.39087.39100';
+UPDATE wilayah SET kode = '33.23.01.2002' WHERE kode_wilayah_kpu = '32676.39436.39437.39439';
+UPDATE wilayah SET kode = '33.23.01.2012' WHERE kode_wilayah_kpu = '32676.39436.39437.39449';
+UPDATE wilayah SET kode = '33.23.03.1001' WHERE kode_wilayah_kpu = '32676.39436.39471.39472';
+UPDATE wilayah SET kode = '33.23.03.1002' WHERE kode_wilayah_kpu = '32676.39436.39471.39473';
+UPDATE wilayah SET kode = '33.23.19.2002' WHERE kode_wilayah_kpu = '32676.39436.39721.39723';
+UPDATE wilayah SET kode = '33.24.06.2016' WHERE kode_wilayah_kpu = '32676.39746.39823.39839';
+UPDATE wilayah SET kode = '33.24.12.2011' WHERE kode_wilayah_kpu = '32676.39746.39912.39923';
+UPDATE wilayah SET kode = '33.24.15.1007' WHERE kode_wilayah_kpu = '32676.39746.39964.39971';
+UPDATE wilayah SET kode = '33.24.16.2014' WHERE kode_wilayah_kpu = '32676.39746.39985.39999';
+UPDATE wilayah SET kode = '33.24.17.2004' WHERE kode_wilayah_kpu = '32676.39746.40002.40006';
+UPDATE wilayah SET kode = '33.24.18.2002' WHERE kode_wilayah_kpu = '32676.39746.40018.40020';
+UPDATE wilayah SET kode = '33.24.18.2012' WHERE kode_wilayah_kpu = '32676.39746.40018.40030';
+UPDATE wilayah SET kode = '33.25.02.2015' WHERE kode_wilayah_kpu = '32676.40053.40070.40083';
+UPDATE wilayah SET kode = '33.26.02.2004' WHERE kode_wilayah_kpu = '32676.40317.40333.40337';
+UPDATE wilayah SET kode = '33.26.02.2005' WHERE kode_wilayah_kpu = '32676.40317.40333.40338';
+UPDATE wilayah SET kode = '33.26.03.2001' WHERE kode_wilayah_kpu = '32676.40317.40349.40350';
+UPDATE wilayah SET kode = '33.26.07.2007' WHERE kode_wilayah_kpu = '32676.40317.40397.40404';
+UPDATE wilayah SET kode = '33.26.08.2006' WHERE kode_wilayah_kpu = '32676.40317.40413.40419';
+UPDATE wilayah SET kode = '33.26.10.2007' WHERE kode_wilayah_kpu = '32676.40317.40463.40471';
+UPDATE wilayah SET kode = '33.26.11.2008' WHERE kode_wilayah_kpu = '32676.40317.40481.40489';
+UPDATE wilayah SET kode = '33.26.11.2021' WHERE kode_wilayah_kpu = '32676.40317.40481.40502';
+UPDATE wilayah SET kode = '33.26.12.2013' WHERE kode_wilayah_kpu = '32676.40317.40504.40517';
+UPDATE wilayah SET kode = '33.27.01.2010' WHERE kode_wilayah_kpu = '32676.40622.40623.40633';
+UPDATE wilayah SET kode = '33.27.06.2016' WHERE kode_wilayah_kpu = '32676.40622.40696.40712';
+UPDATE wilayah SET kode = '33.28.03.2005' WHERE kode_wilayah_kpu = '32676.40859.40893.40898';
+UPDATE wilayah SET kode = '33.28.03.2013' WHERE kode_wilayah_kpu = '32676.40859.40893.40906';
+UPDATE wilayah SET kode = '33.28.04.2016' WHERE kode_wilayah_kpu = '32676.40859.40911.40927';
+UPDATE wilayah SET kode = '33.28.05.2003' WHERE kode_wilayah_kpu = '32676.40859.40932.40935';
+UPDATE wilayah SET kode = '33.28.07.2012' WHERE kode_wilayah_kpu = '32676.40859.40962.40974';
+UPDATE wilayah SET kode = '33.28.09.2021' WHERE kode_wilayah_kpu = '32676.40859.40991.41012';
+UPDATE wilayah SET kode = '33.28.11.2019' WHERE kode_wilayah_kpu = '32676.40859.41026.41045';
+UPDATE wilayah SET kode = '33.28.17.2012' WHERE kode_wilayah_kpu = '32676.40859.41141.41153';
+UPDATE wilayah SET kode = '33.29.13.2006' WHERE kode_wilayah_kpu = '32676.41165.41381.41387';
+UPDATE wilayah SET kode = '33.74.01.1014' WHERE kode_wilayah_kpu = '32676.41585.41586.41600';
+UPDATE wilayah SET kode = '33.74.01.1015' WHERE kode_wilayah_kpu = '32676.41585.41586.41601';
+UPDATE wilayah SET kode = '33.74.07.1004' WHERE kode_wilayah_kpu = '32676.41585.41658.41662';
+UPDATE wilayah SET kode = '33.74.08.1007' WHERE kode_wilayah_kpu = '32676.41585.41669.41676';
+UPDATE wilayah SET kode = '33.74.09.1002' WHERE kode_wilayah_kpu = '32676.41585.41677.41679';
+UPDATE wilayah SET kode = '33.74.15.1003' WHERE kode_wilayah_kpu = '32676.41585.41760.41763';
+
